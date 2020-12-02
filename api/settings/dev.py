d26972ddf478e4b3050e265ba5850799167750df
RobsonMaestro <robson_maestro@alumni.usp.br>
2020-12-02 09:18:12 -0300

    arrumando o dev.py

--- api/settings/dev.py
+++ api/settings/dev.py
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# Acrescentar ao final do arquivo
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
